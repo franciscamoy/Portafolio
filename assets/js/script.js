552533d37756f80a31009da36f9a244a338b0b96
franciscamoy <60185975+franciscamoy@users.noreply.github.com>
2020-01-18 12:42:35 -0400

    ultimos cambios
    funciona mal carusel
    y textos no responsivos

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+$('.carousel').carousel()
+
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+  })
+
+$('#myCarousel').on('slide.bs.carousel', function () {
+    // do something...
+  })
